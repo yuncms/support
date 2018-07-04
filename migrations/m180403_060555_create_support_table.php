6cbd4e3c4fb28cf580aacbeeaf37f59b4e6e94a5
Xu Tongle <xutongle@gmail.com>
2018-07-04 10:31:29 +0800

    utf8 > utf8mb4

--- migrations/m180403_060555_create_support_table.php
+++ migrations/m180403_060555_create_support_table.php
@@ -20,7 +20,7 @@ class m180403_060555_create_support_table extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
         }
         /**
          * 赞表
